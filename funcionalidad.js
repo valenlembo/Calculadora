e2803e1ad2651e80206a21336f13658eecf31f1b
valenlembo <valenlembo99@icloud.comgit config --global user.email valenlembo99@icloud.comgit config --global user.email valenlembo99@icloud.com>
2022-01-25 19:20:25 -0300

    .

---
+++ funcionalidad.js
@@ -0,0 +1,119 @@
+var operandoa;
+var operandob;
+var operacion;
+
+function init(){
+    //variables
+    var resultado = document.getElementById("resultado");
+    var reset = document.getElementById("reset");
+    var suma = document.getElementById("suma");
+    var resta = document.getElementById("resta");
+    var multiplicacion = document.getElementById("multiplicacion");
+    var division = document.getElementById("division");
+    var igual = document.getElementById("igual");
+    var uno = document.getElementById("uno");
+    var dos = document.getElementById("dos");
+    var tres = document.getElementById("tres");
+    var cuatro = document.getElementById("cuatro");
+    var cinco = document.getElementById("cinco");
+    var seis = document.getElementById("seis");
+    var siete = document.getElementById("siete");
+    var ocho = document.getElementById("ocho");
+    var nueve = document.getElementById("nueve");
+    var cero = document.getElementById("cero");
+
+    //eventos
+    uno.onclick = function(e){
+        resultado.textContent = resultado.textContent + "1";
+    }
+    dos.onclick = function(e){
+        resultado.textContent = resultado.textContent + "2";
+    }
+    tres.onclick = function(e){
+        resultado.textContent = resultado.textContent + "3";
+    }
+    cuatro.onclick = function(e){
+        resultado.textContent = resultado.textContent + "4";
+    }
+    cinco.onclick = function(e){
+        resultado.textContent = resultado.textContent + "5";
+    }
+    seis.onclick = function(e){
+        resultado.textContent = resultado.textContent + "6";
+    }
+    siete.onclick = function(e){
+        resultado.textContent = resultado.textContent + "7";
+    }
+    ocho.onclick = function(e){
+        resultado.textContent = resultado.textContent + "8";
+    }
+    nueve.onclick = function(e){
+        resultado.textContent = resultado.textContent + "9";
+    }
+    cero.onclick = function(e){
+        resultado.textContent = resultado.textContent + "0";
+    }
+    /*Aqui arriba lo que le estamos diciendo es que cuando alguien presione
+     el boton 1 por ej lo que va a pasar es que la pantalla de resultado va a 
+     ser igual a lo que ya tenia, es decir a resultado.TextContent mas el numero 
+     1, y no que se borre lo anterior */    
+
+    reset.onclick = function(e){
+        resetear();
+    }
+    suma.onclick = function(e){
+        operandoa = resultado.textContent;
+        operacion = "+";
+        limpiar();
+    }
+    resta.onclick = function(e){
+        operandoa = resultado.textContent;
+        operacion = "-";
+        limpiar();
+    }
+    multiplicacion.onclick = function(e){
+        operandoa = resultado.textContent;
+        operacion = "*";
+        limpiar();
+    }
+    division.onclick = function(e){
+        operandoa = resultado.textContent;
+        operacion = "/";
+        limpiar();
+    }
+    igual.onclick = function(e){
+        operandob = resultado.textContent;
+        resolver();
+    }
+}
+
+function limpiar(){
+    resultado.textContent = "";
+}
+
+function resetear(){
+    resultado.textContent = "";
+    operandoa = 0;
+    operandob = 0;
+    operacion = "";
+}
+
+function resolver(){
+    var res = 0;
+    switch(operacion){
+        case "+":
+            res = parseFloat(operandoa) + parseFloat(operandob);
+            break;
+        case "-":
+            res = parseFloat(operandoa) - parseFloat(operandob);
+            break;
+        case "*":
+            res = parseFloat(operandoa) * parseFloat(operandob);
+            break;
+        case "/":
+            res = parseFloat(operandoa) / parseFloat(operandob);
+            break;
+    }
+    resetear();
+    resultado.textContent = res;
+}
